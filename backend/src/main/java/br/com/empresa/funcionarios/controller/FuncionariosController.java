00415ece2d3294ae215985a4592af8972bedacb9
Helden de Siqueira Cordeiro <siqueirahelden@gmail.com>
2020-03-22 23:26:34 -0300

    implementando o controller

--- backend/src/main/java/br/com/empresa/funcionarios/controller/FuncionariosController.java
+++ backend/src/main/java/br/com/empresa/funcionarios/controller/FuncionariosController.java
@@ -1,5 +1,24 @@
 package br.com.empresa.funcionarios.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.empresa.funcionarios.model.Funcionarios;
+
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1")
 public class FuncionariosController {
 
-}
+
+	@GetMapping("/funcionarios")
+	public List<Funcionarios> getAllFuncionarios() {
+		
+	}
